472dd7aefcb6ba435ec42e50eddda0ebd5e791a4
Betty Chen <s9654025@gmail.com>
2019-08-04 21:29:59 +0800

    [Add] tracks' photo and music

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const tracks = [
     id: 1,
     title: 'Dawn_of_Man',
     album: 'Quincas Moreira',
-    img: 'https://picsum.photos/1000',
+    img: './tracks/01.jpg',
     time: 127,
     isFav: false,
     lyrics: 'No lyrics'
@@ -15,7 +15,7 @@ const tracks = [
     id: 2,
     title: 'Metamorphosis',
     album: 'Quincas Moreira',
-    img: '02',
+    img: './tracks/02.jpg',
     time: 171,
     isFav: false,
     lyrics: 'No lyrics'
@@ -24,7 +24,7 @@ const tracks = [
     id: 3,
     title: 'Slowly_Until_We_Get_There',
     album: 'Joey Pecoraro',
-    img: '03',
+    img: './tracks/03.jpg',
     time: 98,
     isFav: false,
     lyrics: 'No lyrics'
@@ -33,7 +33,7 @@ const tracks = [
     id: 4,
     title: 'Jessica',
     album: 'Joey Pecoraro',
-    img: '04',
+    img: './tracks/04.jpg',
     time: 167,
     isFav: false,
     lyrics: 'No lyrics'
@@ -42,7 +42,7 @@ const tracks = [
     id: 5,
     title: 'For_We_Are_Many',
     album: 'Cooper Cannell',
-    img: '05',
+    img: './tracks/05.jpg',
     time: 173,
     isFav: false,
     lyrics: 'No lyrics'
